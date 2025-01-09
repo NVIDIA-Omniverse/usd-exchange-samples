9156769201711a17a2df53ddd3fbcc78977b96d7
YunLiu <55491388+KumoLiu@users.noreply.github.com>
2025-01-09 15:27:18 +0800

    add medical mesh examples
    Signed-off-by: YunLiu <55491388+KumoLiu@users.noreply.github.com>

--- source/common/usdUtils.py
+++ source/common/usdUtils.py
@@ -5,8 +5,12 @@
 import traceback
 from typing import Optional
 
+import sys
+sys.path.append("/home/yunliu/.local/lib/python3.10/site-packages")
+
+import vtk
 import usdex.core
-from pxr import Gf, Sdf, Tf, Usd, UsdGeom
+from pxr import Gf, Sdf, Tf, Usd, UsdGeom, Vt
 
 
 def getSamplesAuthoringMetadata():
@@ -233,5 +237,133 @@ def createCubeMesh(parent: str, meshName: str = "cubeMesh", halfHeight: float = 
             scale=Gf.Vec3f(1),
             time=Usd.TimeCode.Default(),
         )
+    return meshPrim
 
+
+def convert_to_mesh(
+    segmentation_path, label_value=1, smoothing_factor=0.5, reduction_ratio=0.0
+):
+    """
+    Function to perform segmentation-to-mesh conversion and smoothing from NIFTI image
+    """
+    # Step 1: Load segmentation (binary labelmap, e.g., NRRD file)
+    reader = vtk.vtkNIFTIImageReader()
+    reader.SetFileName(segmentation_path)
+    reader.Update()
+
+    # Step 2: Create Closed Surface Representation using vtkDiscreteFlyingEdges3D
+    flying_edges = vtk.vtkDiscreteFlyingEdges3D()
+    flying_edges.SetInputConnection(reader.GetOutputPort())
+    flying_edges.ComputeGradientsOff()
+    flying_edges.ComputeNormalsOff()
+    flying_edges.SetValue(0, label_value)
+    flying_edges.Update()
+
+    if flying_edges.GetOutput().GetNumberOfPoints() == 0:
+        print(f"No points found for label {label_value}. Skipping...")
+        return None
+
+    # Step 3: Decimate the mesh
+    if reduction_ratio > 0.0:
+        decimation_filter = vtk.vtkDecimatePro()
+        decimation_filter.SetInputConnection(flying_edges.GetOutputPort())
+        decimation_filter.SetFeatureAngle(60)
+        decimation_filter.SplittingOff()
+        decimation_filter.PreserveTopologyOn()
+        decimation_filter.SetMaximumError(1)
+        decimation_filter.SetTargetReduction(reduction_ratio)
+        decimation_filter.Update()
+
+    # Step 4: Smooth the resulting mesh
+    smoothing_filter = vtk.vtkWindowedSincPolyDataFilter()
+    numberOfIterations = int(20 + smoothing_factor * 40)
+    passBand = pow(10.0, -4.0 * smoothing_factor)
+    if reduction_ratio > 0.0:
+        smoothing_filter.SetInputConnection(decimation_filter.GetOutputPort())
+    else:
+        smoothing_filter.SetInputConnection(flying_edges.GetOutputPort())
+    smoothing_filter.SetNumberOfIterations(numberOfIterations)
+    smoothing_filter.SetPassBand(passBand)
+    smoothing_filter.BoundarySmoothingOff()
+    smoothing_filter.FeatureEdgeSmoothingOff()
+    smoothing_filter.NonManifoldSmoothingOn()
+    smoothing_filter.NormalizeCoordinatesOn()
+    smoothing_filter.Update()
+
+    # Step 5: Decimate the mesh further
+    decimation = vtk.vtkQuadricDecimation()
+    decimation.SetInputConnection(smoothing_filter.GetOutputPort())
+    decimation.SetTargetReduction(0.9)  # 90% reduction, the same as slicer
+    decimation.VolumePreservationOn()
+    decimation.Update()
+
+    # Step 6: Generate normals for better shading
+    decimatedNormals = vtk.vtkPolyDataNormals()
+    decimatedNormals.SetInputConnection(decimation.GetOutputPort())
+    decimatedNormals.SplittingOff()
+    decimatedNormals.ConsistencyOn()
+    decimatedNormals.Update()
+
+    # Step 7: convert to LPS
+    ras2lps = vtk.vtkMatrix4x4()
+    ras2lps.SetElement(0, 0, -1)
+    ras2lps.SetElement(1, 1, -1)
+    ras2lpsTransform = vtk.vtkTransform()
+    ras2lpsTransform.SetMatrix(ras2lps)
+    transformer = vtk.vtkTransformPolyDataFilter()
+    transformer.SetTransform(ras2lpsTransform)
+    transformer.SetInputConnection(decimatedNormals.GetOutputPort())
+    transformer.Update()
+    
+    return transformer
+
+
+def createMeshFromVtk(parent, meshName, vtkData, color, translation=Gf.Vec3d(0.0), rotation=Gf.Vec3f(0.0, 0.0, 180.0)):
+    meshPrimPath = parent.GetPath().AppendChild(meshName)
+    print(f"Creating mesh at path: {meshPrimPath}")
+    
+    # Create a new USD mesh under the parent
+    meshPrim = UsdGeom.Mesh.Define(parent.GetStage(), meshPrimPath)
+    
+    # Extract points and polygons from the VTK data
+    points = vtkData.GetPoints()
+    polys = vtkData.GetPolys()
+    
+    # Convert VTK points to USD points
+    usdPoints = []
+    for i in range(points.GetNumberOfPoints()):
+        p = points.GetPoint(i)
+        usdPoints.append(Gf.Vec3f(p[0], p[1], p[2]))
+    
+    # Convert VTK polygons to USD face vertex indices
+    usdFaceVertexIndices = []
+    usdFaceVertexCounts = []
+    polys.InitTraversal()
+    idList = vtk.vtkIdList()
+    while polys.GetNextCell(idList):
+        usdFaceVertexCounts.append(idList.GetNumberOfIds())
+        for j in range(idList.GetNumberOfIds()):
+            usdFaceVertexIndices.append(idList.GetId(j))
+    
+    # Set the points and face vertex indices on the USD mesh
+    meshPrim.GetPointsAttr().Set(usdPoints)
+    meshPrim.GetFaceVertexIndicesAttr().Set(usdFaceVertexIndices)
+    meshPrim.GetFaceVertexCountsAttr().Set(usdFaceVertexCounts)
+    
+    color = [Gf.Vec3f(color)]
+    primvar = meshPrim.GetDisplayColorPrimvar()
+    usdex.core.Vec3fPrimvarData(UsdGeom.Tokens.constant, color).setPrimvar(primvar)
+    
+    # Set initial transformation if localPos != 0,0,0
+    if translation != Gf.Vec3d(0.0):
+        usdex.core.setLocalTransform(
+            prim=meshPrim.GetPrim(),
+            translation=translation,
+            pivot=Gf.Vec3d(0.0),
+            rotation=rotation,
+            rotationOrder=usdex.core.RotationOrder.eXyz,
+            scale=Gf.Vec3f(1),
+            time=Usd.TimeCode.Default(),
+        )
+    
     return meshPrim
